6a666a057532e2280c0d5032d1c9dc381a74c213
Andrew <siminiucandrew@gmail.com>
2020-05-24 01:16:08 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log((function doubleNumber(num) {
+    return num * 2;
+})(10)); // (20)
+
+//IIFE - Immediately Invoked Function Expression (A function that is executed right after it is created)
